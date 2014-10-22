202510773ce4726eae80306867b6a2a278c00e45
Benjamin Calderon <benj.calderon@gmail.com>
2014-10-22 10:24:36 -0400

    cleaned up code, starting rewrite

--- greekify.js
+++ greekify.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-var file = fs.readFileSync(process.argv[2], 'utf8'); //process.argv[2] is the file input
+var str = fs.readFileSync(process.argv[2], 'utf8'); //process.argv[2] is the file input
 
 // Greek Unicode decimal values using the method charCodeAt()
 // \u0370 == 880
@@ -9,21 +9,21 @@ var file = fs.readFileSync(process.argv[2], 'utf8'); //process.argv[2] is the fi
 // \u1F00 == 7936
 // \u1FFF == 8191
 
-function isGreek(str, x) {
-  if ((str.charCodeAt(x) >= 880 && str.charCodeAt(x) <= 1023) || (str.charCodeAt(x) >= 7936 && str.charCodeAt(x) <= 8191)) {
-    return true;
-  } else
-    return false;
+function isGreekChar(i) {
+  if ((i.charCodeAt(0) >= 880 && i.charCodeAt(0) <= 1023) || (i.charCodeAt(0) >= 7936 && i.charCodeAt(0) <= 8191)) {
+  return true;
+} else
+  return false;
 };
 
 var new_str = ''
 
-for (i = 0; i < file.length; i++) {
-  if (isGreek(file, i)) {
-    new_str += ("<span class=\"greekFont\">" + file[i] + "</span>");
+for (i = 0; i < str.length; i++) {
+  if (isGreek(str, i)) {
+    new_str += ("<span class=\"greekFont\">" + str[i] + "</span>");
   } else {
-    new_str += file[i];
+    new_str += str[i];
   }
 }
 
-fs.writeFileSync(process.argv[3], new_str);
+// fs.writeFileSync(process.argv[3], new_str);

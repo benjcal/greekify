26047d36dc3b87e02fc217755ab35f9a1c7fa99d
Benjamin Calderon <benj.calderon@gmail.com>
2014-10-21 20:11:50 -0400

    first commit

---
+++ greekify.js
@@ -0,0 +1,29 @@
+var fs = require('fs');
+
+var file = fs.readFileSync(process.argv[2], 'utf8'); //process.argv[2] is the file input
+
+// Greek Unicode decimal values using the method charCodeAt()
+// \u0370 == 880
+// \u03FF == 1023
+
+// \u1F00 == 7936
+// \u1FFF == 8191
+
+function checkGreek(str, x) {
+  if ((str.charCodeAt(x) >= 880 && str.charCodeAt(x) <= 1023) || (str.charCodeAt(x) >= 7936 && str.charCodeAt(x) <= 8191)) {
+    return true;
+  } else
+    return false;
+};
+
+var new_str = ''
+
+for (i = 0; i < file.length; i++) {
+  if (checkGreek(file, i)) {
+    new_str += ("<span class=\"greekFont\">" + file[i] + "</span>");
+  } else {
+    new_str += file[i];
+  }
+}
+
+fs.writeFileSync(process.argv[3], new_str);

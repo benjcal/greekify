700e2a38d64affa7bdbfb791e1d4ac93eed745db
Benjamin Calderon <benj.calderon@gmail.com>
2014-10-22 13:23:38 -0400

    updates for today...

--- greekify.js
+++ greekify.js
@@ -1,6 +1,8 @@
+"use strict";
+
 var fs = require('fs');
 
-var str = fs.readFileSync(process.argv[2], 'utf8'); //process.argv[2] is the file input
+// var str = fs.readFileSync(process.argv[2], 'utf8'); //process.argv[2] is the file input
 
 // Greek Unicode decimal values using the method charCodeAt()
 // \u0370 == 880
@@ -9,21 +11,58 @@ var str = fs.readFileSync(process.argv[2], 'utf8'); //process.argv[2] is the fil
 // \u1F00 == 7936
 // \u1FFF == 8191
 
-function isGreekChar(i) {
-  if ((i.charCodeAt(0) >= 880 && i.charCodeAt(0) <= 1023) || (i.charCodeAt(0) >= 7936 && i.charCodeAt(0) <= 8191)) {
-  return true;
-} else
-  return false;
+
+var demoStr = '\u0370\u0370\u0370';
+
+
+
+/*****
+Receives a string and a location a returns true if the character
+in that location is a greek character.
+*****/
+function isGreekChar(str, x) {
+  if ((str.charCodeAt(x) >= 880 &&
+      str.charCodeAt(x) <= 1023) ||
+    (str.charCodeAt(x) >= 7936 &&
+      str.charCodeAt(x) <= 8191)) {
+    return true;
+  } else
+    return false;
 };
 
-var new_str = ''
+/*****
+Receives a string and a location and returns how many greek character are
+*****/
+function howLong(str, x) {
+  var n = 0;
 
-for (i = 0; i < str.length; i++) {
-  if (isGreek(str, i)) {
-    new_str += ("<span class=\"greekFont\">" + str[i] + "</span>");
-  } else {
-    new_str += str[i];
+
+  while (flag) {
+    if (isGreekChar(str, x + 1)) {
+      n += 1;
+      x += 1;
+      continue;
+    }
+    flag = false
+    console.log('lol');
   }
+
 }
 
-// fs.writeFileSync(process.argv[3], new_str);
+var flag = true;
+var x = 0;
+howLong(demoStr, x)
+
+
+
+// var new_str = ''
+//
+// for (i = 0; i < str.length; i++) {
+//   if (isGreek(str, i)) {
+//     new_str += ("<span class=\"greekFont\">" + str[i] + "</span>");
+//   } else {
+//     new_str += str[i];
+//   }
+// }
+//
+// // fs.writeFileSync(process.argv[3], new_str);

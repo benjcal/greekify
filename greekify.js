dd9df1db707153b20f90f2fd5df3e859b423e9d0
Benjamin Calderon <benj.calderon@gmail.com>
2014-10-22 20:27:08 -0400

    removed var demoStr

--- greekify.js
+++ greekify.js
@@ -11,8 +11,6 @@ var str = fs.readFileSync(process.argv[2], 'utf8'); //process.argv[2] is the fil
 // \u1F00 == 7936
 // \u1FFF == 8191
 
-var demoStr = "There are three uses of the middle voice. The Reflexive Middle reflects the action \non the subject—ὁ ἄνθρωπος νίπτεται - \"the man is washing himself.\" The Intensive Middle stresses \nthe agent producing the action. It says this is the person and no other who is performing the \naction—ὁ κυρίος δοξάζεται τὴν ἐκκλησίαν - \"The Lord is glorifying the church.\" The point is that \nit is He and not other who is accomplishing the task. The Reciprocal Middle features the \ninterchange of the action among plural subjects—αἱ προφήται διδάσκονται - \"The prophets are \nteaching each other.\"";
-
 /*** Receives a string and a location a returns true if the character
 in that location is a greek character ***/
 function isGreekChar(str, loc) {
@@ -27,11 +25,9 @@ function isGreekChar(str, loc) {
 
 /*** Receives a string and a location and returns how many greek character are ***/
 function isNext(str, loc) {
-
   if (isGreekChar(str, loc + 1)) { //this is true if the next char IS a greek char
     return true;
   }
-
   if (!isGreekChar(str, loc + 1)) { //this is true if the next char is NOT a greek char
     if (isGreekChar(str, loc + 2)) {
       return true;
@@ -67,8 +63,7 @@ function main(inStr) {
       tmpStr += inStr[i]
     }
   }
-
   return tmpStr;
 }
 
-fs.writeFileSync(process.argv[2]  += '.greekify', main(str));
+fs.writeFileSync(process.argv[2] += '.greekify', main(str));
